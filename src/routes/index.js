d6253c7860c049add02c7349312b955c3c230089
Dyanika <pyanika.pu@gmail.com>
2022-05-07 15:40:56 +0700

    Get petition and prisma db

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const authController = require('./auth.controller')
+const petition = require('./petition')
 
 router.use(authController)
+router.use(petition)
 
 router.get("/",(req,res)=>{
     return res.send({api:"SAMOSIT"})
