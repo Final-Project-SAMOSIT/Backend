63f3e1adb3a9cb0551c7c2e0093b7fff7c863b24
Dyanika <pyanika.pu@gmail.com>
2022-05-10 17:21:09 +0700

    Edit database and add petType&status

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 const authController = require('./auth.controller')
-const petition = require('./petition')
+const petitionType = require('./petitionType')
+const petitionStatus = require('./petitionStatus')
 
 router.use(authController)
-router.use(petition)
+router.use(petitionType)
+router.use(petitionStatus)
 
 router.get("/",(req,res)=>{
     return res.send({api:"SAMOSIT"})
