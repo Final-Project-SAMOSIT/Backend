21b637e9f19c8189e3342deb110a239f65f6a772
ppitcahnon <pitchanon2544@gmail.com>
2022-10-11 20:02:16 +0700

    [Fix]image size

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const app = express()
 require("dotenv").config()
 
 // Configure express app
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
 // Add app listening port
 const PORT = process.env.PORT || 4000
